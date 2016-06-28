6913a4ab9016453f449bd21d73b45703eb6cf457
aegistudio <1910567646@qq.com>
2016-06-28 20:36:41 +0800

    Initial commit.

---
+++ useless.hs
@@ -0,0 +1,23 @@
+-- we are likely to seperate something from
+-- a structure, where a bi-function is ideal.
+bi l r f x = f (l x) (r x)
+
+-- seperate first element from a list.
+-- easily construct right-recursive function.
+pop = bi head tail
+
+-- seperate last element from a list.
+-- this function is strongly deprecated!
+-- easily construct left-recursive function.
+popt = bi (reverse.tail.reverse) last
+popTail = popt
+
+-- seperate first element from second in a tuple.
+-- also serves as a unzip function (from a zip)
+tmap = bi fst snd
+
+-- i think you should use && and || instead
+-- but who knows. somebody would love a `and` b
+-- or a `or` b, like me.
+and a b = if a then b else False
+or a b = if a then True else b
